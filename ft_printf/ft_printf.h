47d25e8e08f332a41ed4118f35874cbca18ae424
Kazuki Adachi <kadachi@student.42tokyo.jp>
2024-12-21 14:02:13 +0900

    [ADD] ft_vdprintf()

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kadachi <kadachi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 19:33:47 by kadachi           #+#    #+#             */
-/*   Updated: 2024/12/09 16:36:40 by kadachi          ###   ########.fr       */
+/*   Updated: 2024/12/21 13:58:47 by kadachi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 
 int		ft_printf(const char *format, ...);
 int		ft_dprintf(int fd, const char *format, ...);
+int		ft_vdprintf(int fd, const char *format, va_list argp);
 int		print_char(int fd, int c);
 int		print_str(int fd, char *str);
 int		print_ptr(int fd, unsigned long ptr);
